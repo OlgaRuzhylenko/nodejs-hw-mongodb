a2d74c3061381f1fa519b2313bbd6f577ae52804
Olga <olyaruzhylenko@gmail.com>
2024-07-04 12:14:44 +0300

    filters done

--- src/utils/parseContactsFilterParams.js
+++ src/utils/parseContactsFilterParams.js
@@ -1,12 +1,21 @@
 import { contactTypeList } from '../constants/contacts-constants.js';
 
-const parsedBoolean = (value) => {
-  if (typeof value !== 'string') return;
-  if (!['true', 'false'].includes(value)) return;
+// const parsedBoolean = (value) => {
+//   if (typeof value !== 'string') return;
+//   if (!['true', 'false'].includes(value)) return;
 
-  const parsedValue = Boolean(value);
-  return parsedValue;
+//   const parsedValue = Boolean(value);
+//   return parsedValue;
+// };
+
+const parsedBoolean = (isFavourite) => {
+  const isBoolean = isFavourite === 'true' || isFavourite === 'false';
+
+  if (!isBoolean) return;
+
+  return isFavourite === 'true' ? true : false;
 };
+
 
 export const parseContactsFilterParams = ({ contactType, isFavourite }) => {
   const parsedType = contactTypeList.includes(contactType) ? contactType : null;
