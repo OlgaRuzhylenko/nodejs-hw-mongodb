6020e2a0c57155035109bd1847c24cacc612f1f2
Olga <olyaruzhylenko@gmail.com>
2024-07-18 20:29:49 +0300

    прописано getAll

--- src/routers/auth-router.js
+++ src/routers/auth-router.js
@@ -3,49 +3,23 @@ import { Router } from 'express';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import validateBody from '../utils/validateBody.js';
 
-import {
-  userSignupSchema,
-  userSigninSchema,
-} from '../validation/user-schema.js';
-import {
-  loginController,
-  signupController,
-  refreshController,
-  logoutController,
-  requestResetEmailController,
-  resetPasswordController,
-} from '../controllers/auth_controllers.js';
+import { userSignupSchema, userSigninSchema,} from '../validation/user-schema.js';
+import {loginController, signupController, refreshController, logoutController, requestResetEmailController, resetPasswordController,} from '../controllers/auth_controllers.js';
 import { requestResetEmailSchema } from '../validation/requestResetEmailSchema.js';
 import { resetPasswordSchema } from '../validation/resetPasswordSchema.js';
 
 const authRouter = Router();
 
-authRouter.post(
-  '/register',
-  validateBody(userSignupSchema),
-  ctrlWrapper(signupController),
-);
+authRouter.post( '/register', validateBody(userSignupSchema), ctrlWrapper(signupController),);
 
-authRouter.post(
-  '/login',
-  validateBody(userSigninSchema),
-  ctrlWrapper(loginController),
-);
+authRouter.post( '/login', validateBody(userSigninSchema), ctrlWrapper(loginController),);
 
 authRouter.post('/refresh', ctrlWrapper(refreshController));
 
 authRouter.post('/logout', ctrlWrapper(logoutController));
 
-authRouter.post(
-  '/send-reset-email',
-  validateBody(requestResetEmailSchema),
-  ctrlWrapper(requestResetEmailController),
-);
+authRouter.post('/send-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController),);
 
-authRouter.post(
-  '/reset-pwd',
-  validateBody(resetPasswordSchema),
-  ctrlWrapper(resetPasswordController),
-);
+authRouter.post('/reset-pwd', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController),);
 
 export default authRouter;
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import env from './utils/env.js';
 import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth-router.js';
 import cookieParser from 'cookie-parser';
-import path from 'node:path'
+import path from 'node:path';
 
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
@@ -24,8 +24,8 @@ const setupServer = () => {
   app.use(express.json());
   app.use(logger);
   app.use(cors());
-app.use(cookieParser());
-app.use(express.static(PUBLIC_DIR));
+  app.use(cookieParser());
+  app.use(express.static(PUBLIC_DIR));
 
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
